a5cc48dce443ece2fb60db58cdec6d2cef06e02c
Ari Summer <aribsummer@gmail.com>
2015-12-12 16:43:58 -0700

    Add vi-mode.zsh for vim customizations

--- cli/zsh/vi-mode.zsh
+++ cli/zsh/vi-mode.zsh
@@ -5,33 +5,34 @@ export VISUAL=vim
 # Shorten key timeout to 10 ms
 # export KEYTIMEOUT=10
 
-# # Use jk as escape in shell
-# bindkey -M viins 'jk' vi-cmd-mode
+# Use jk as escape in shell
+bindkey -M viins 'jk' vi-cmd-mode
+bindkey -M viins 'jj' vi-cmd-mode
 
-# # Surround
-# autoload -Uz surround
-# zle -N delete-surround surround
-# zle -N add-surround surround
-# zle -N change-surround surround
-# bindkey -a cs change-surround
-# bindkey -a ds delete-surround
-# bindkey -a ys add-surround
-# bindkey -M visual S add-surround
+# Surround
+autoload -Uz surround
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+bindkey -a cs change-surround
+bindkey -a ds delete-surround
+bindkey -a ys add-surround
+bindkey -M visual S add-surround
 
-# # Select bracketed
-# autoload -U select-bracketed
-# zle -N select-bracketed
-# for m in visual viopp; do
-#   for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
-#     bindkey -M $m $c select-bracketed
-#   done
-# done
+# Select bracketed
+autoload -U select-bracketed
+zle -N select-bracketed
+for m in visual viopp; do
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+    bindkey -M $m $c select-bracketed
+  done
+done
 
-# # Surround quoted
-# autoload -U select-quoted
-# zle -N select-quoted
-# for m in visual viopp; do
-#   for c in {a,i}{\',\",\`}; do
-#     bindkey -M $m $c select-quoted
-#   done
-# done
+# Surround quoted
+autoload -U select-quoted
+zle -N select-quoted
+for m in visual viopp; do
+  for c in {a,i}{\',\",\`}; do
+    bindkey -M $m $c select-quoted
+  done
+done
