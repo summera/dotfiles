4d2da6ce7eb3dd19df51af82455f92af91493a7e
Ari Summer <aribsummer@gmail.com>
2024-03-01 18:45:42 -0700

    Remove unneeded vim-tmux-yank

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -2,5 +2,4 @@ return {
   "AstroNvim/astrocommunity",
 
   -- Available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  { import = "astrocommunity.terminal-integration.vim-tmux-yank" }
 }
