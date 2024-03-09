0a06d1f6628b4c2b9a9e44cefe879c1335f2a9e6
Ari Summer <aribsummer@gmail.com>
2024-03-09 11:49:19 -0700

    Prevent cursor repositioning on refocus

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -33,4 +33,21 @@ return {
     -- Changes the paste command in visual mode so
     -- that it doesn't overwrite whatever is in your paste buffer.
     keymap("v", "p", '"_dP', { noremap = true, silent = true })
+
+    -- Prevent moving cursor on window focus
+    vim.api.nvim_create_autocmd("FocusGained", {
+      callback = function()
+        -- Wait 100ms before re-enabling to avoid
+        -- unwanted cursor repositioning
+        vim.fn.timer_start(100, function()
+          vim.o.mouse = "a"
+        end)
+      end
+    })
+    vim.api.nvim_create_autocmd("FocusLost", {
+      callback = function()
+        vim.o.mouse = ""
+      end
+    })
+  end,
 }
--- dot_tmux.zsh
+++ dot_tmux.zsh
@@ -1,14 +1,3 @@
-# Taken from https://github.com/sorin-ionescu/prezto/blob/master/modules/tmux/init.zsh
-
 if [[ -z "$TMUX" && -z "$EMACS" && -z "$VIM" && -z "$INSIDE_EMACS" && "$TERM_PROGRAM" != "vscode" && "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ]]; then
-  tmux start-server
-
-  if ! tmux has-session 2> /dev/null; then
-    tmux \
-      new-session -d -s base \; \
-      set-option -t base destroy-unattached off &> /dev/null
-  fi
-
-  # Attach to the 'base' session or to the last session used. (detach first)
-  exec tmux attach-session -d
+  tmux attach -t base || tmux new -s base
 fi
