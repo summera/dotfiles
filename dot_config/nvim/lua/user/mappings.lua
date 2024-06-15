b3683040219218e88e1b95f61f764136c897f562
Ari Summer <aribsummer@gmail.com>
2024-06-14 19:35:11 -0600

    Add --fixed-strings to ripgrep word search

--- dot_config/nvim/lua/user/mappings.lua
+++ dot_config/nvim/lua/user/mappings.lua
@@ -1,6 +1,26 @@
 return {
   n = {
-    [";"] = { ":", desc = "Ex mode" }
+    [";"] = { ":", desc = "Ex mode" },
+    ["<leader>fw"] = {
+      function()
+        require("telescope.builtin").live_grep {
+          additional_args = function(args) 
+            return vim.list_extend(args, { "--fixed-strings" }) 
+          end,
+        }
+      end,
+      desc="Find words"
+    },
+    ["<leader>fW"] = {
+      function()
+        require("telescope.builtin").live_grep {
+          additional_args = function(args) 
+            return vim.list_extend(args, { "--hidden", "--no-ignore", "--fixed-strings" }) 
+          end,
+        }
+      end,
+      desc = "Find words in all files",
+    }
   },
   v = {
     [";"] = { ":", desc = "Ex mode" }
