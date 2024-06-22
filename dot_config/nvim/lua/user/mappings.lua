2a8034952a26c95ed7bf3d8a012a1eb617720309
Ari Summer <aribsummer@gmail.com>
2024-06-21 18:30:25 -0600

    Disable telescope and add more fzf bindings

--- dot_config/nvim/lua/user/mappings.lua
+++ dot_config/nvim/lua/user/mappings.lua
@@ -1,24 +1,78 @@
 return {
   n = {
     [";"] = { ":", desc = "Ex mode" },
-    ["<leader>fw"] = {
+
+    -- Files
+    ["<leader>fs"] = {
       function()
         require("fzf-lua").grep_project()
       end,
-      desc="Find words"
+      desc = "Find words"
     },
-    ["<leader>fW"] = {
+    ["<leader>fS"] = {
       function()
         require("fzf-lua").grep_project({
           rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e"
         })
       end,
-      desc = "Find words in all files",
-    }
+      desc = "Find words in all files (including hidden)",
+    },
+    ["<leader>f/"] = { 
+      function() 
+        require("fzf-lua").grep_curbuf() 
+      end, 
+      desc = "Find words in current buffer" 
+    },
+    ["<leader>ff"] = {
+      function()
+        require("fzf-lua").files()
+      end, 
+      desc = "Find files"
+    },
+    ["<leader>fb"] = { 
+      function() 
+        require("fzf-lua").buffers()
+      end, 
+      desc = "Find buffers" 
+    },
+    ["<leader>fk"] = { 
+      function() 
+        require("fzf-lua").keymaps() 
+      end, 
+      desc = "Find keymaps" 
+    },
+    ["<leader>fh"] = { 
+      function() 
+        require("fzf-lua").helptags() 
+      end, 
+      desc = "Find help" 
+    },
+    ["<leader>f'"] = { 
+      function() 
+        require("fzf-lua").marks() 
+      end, 
+      desc = "Find marks" 
+    },
+
+    -- Git
+    ["<leader>gb"] = { 
+      function() 
+        require("fzf-lua").git_branches({ preview = "git log --color {1}" })
+      end, 
+      desc = "Git branches" 
+    },
+    ["<leader>gt"] = { 
+      function() 
+        require("fzf-lua").git_tags() 
+      end, 
+      desc = "Git tags" 
+    },
   },
+  
   v = {
     [";"] = { ":", desc = "Ex mode" }
   },
+  
   i = {
     ["<C-l>"] = { "<Right>", desc = "Arrow right" },
     ["<C-h>"] = { "<Left>", desc = "Arrow left" },
--- dot_config/nvim/lua/user/plugins/fzf.lua
+++ dot_config/nvim/lua/user/plugins/fzf.lua
@@ -1,7 +1,22 @@
 return {
   "ibhagwan/fzf-lua",
   -- optional for icon support
-  dependencies = {
-    "nvim-tree/nvim-web-devicons"
+  dependencies = { 
+    "nvim-tree/nvim-web-devicons" 
   },
+  config = function()
+    require("fzf-lua").setup({ 
+      fzf_colors = true,
+      winopts = { 
+        width = 0.95, 
+        height = 0.95,
+        preview = { wrap = "wrap" }
+      },
+      previewers = {
+        builtin = {
+          hl_cursorline = "Search"
+        }
+      }
+    })
+  end
 }
