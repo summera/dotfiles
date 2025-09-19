535c9651a07b86e5bc6ecc516cfe0a85f4218492
Ari Summer <aribsummer@gmail.com>
2025-09-19 18:28:36 -0400

    Disable snacks scroll animation and picker wrapping

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -3,25 +3,7 @@ return {
 		"folke/snacks.nvim",
 		opts = {
 			dashboard = { enabled = false },
-			picker = {
-				win = {
-					input = {
-						wo = {
-							wrap = true,
-						},
-					},
-					list = {
-						wo = {
-							wrap = true,
-						},
-					},
-					preview = {
-						wo = {
-							wrap = true,
-						},
-					},
-				},
-			},
+			scroll = { enabled = false },
 		},
 	},
 }
